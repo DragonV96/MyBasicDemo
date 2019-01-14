a5d162ec1ec49cf3e9d2ff81d2c5464bdf56e721
DragonV96 <510537547@qq.com>
2019-01-14 22:32:38 +0800

    我的Java基础demo集合

---
+++ collections/src/main/java/com/glw/SetDemo.java
@@ -0,0 +1,8 @@
+package com.glw;
+
+/**
+ * Create by glw
+ * 2019/1/14 22:01
+ */
+public class SetDemo {
+}
---
+++ design-pattern/src/main/java/com/glw/SingletonStatic.java
@@ -0,0 +1,21 @@
+package com.glw;
+
+/**
+ * Create by glw
+ * 2019/1/14 22:18
+ * 静态内部类单例模式
+ * 优点：避免了线程不安全，按需加载，效率高。
+ */
+public class SingletonStatic {
+
+    private SingletonStatic () {}
+
+    private static class Singleton{
+        private static final SingletonStatic INSTANCE = new SingletonStatic();
+    }
+
+    public SingletonStatic getInstance () {
+        return Singleton.INSTANCE;
+    }
+
+}
