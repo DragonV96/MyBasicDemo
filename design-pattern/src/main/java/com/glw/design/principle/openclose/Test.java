82d66a2309cd5ed1808b06d944bbf09a84afaed2
glw <guolongwei@comtom.cn>
2019-09-06 18:39:24 +0800

    【修改】完善开闭原则实例

--- design-pattern/src/main/java/com/glw/design/principle/openclose/Test.java
+++ design-pattern/src/main/java/com/glw/design/principle/openclose/Test.java
@@ -1,18 +1,16 @@
 package com.glw.design.principle.openclose;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * @author : glw
  * @date : 2019/9/6
  * @time : 15:30
  * @Description : 开闭原则：测试类
  */
-@Slf4j
 public class Test {
 
     public static void main(String[] args) {
-        ICourse javaCourse = new JavaCourse(96, "Java从入门到入土", 998D);
-        log.info("课程ID：{}, 课程名称：{}, 课程价格：{}", javaCourse.getId(), javaCourse.getName(), javaCourse.getPrice());
+        ICourse course = new JavaDiscountCourse(96, "Java从入门到入土", 998D);
+        JavaDiscountCourse javaCourse = (JavaDiscountCourse)course;
+        System.out.println("课程ID：" + javaCourse.getId() + ", 课程名称：" + javaCourse.getName() + ", 课程折后价格：" + javaCourse.getPrice() + "元, 课程价格：" + javaCourse.getOriginPrice() + "元");
     }
 }
