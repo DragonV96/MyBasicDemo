0f54cb5fe112509cde93d8640bc56953bbc6c2ec
glw <guolongwei@comtom.cn>
2019-09-16 10:04:23 +0800

    【添加】接口隔离原则实例

---
+++ design-pattern/src/main/java/com/glw/design/principle/interfaceisolation/IEatAnimalAction.java
@@ -0,0 +1,12 @@
+package com.glw.design.principle.interfaceisolation;
+
+/**
+ * @author : glw
+ * @date : 2019/9/16
+ * @time : 10:00
+ * @Description : 接口隔离原则：动物进食行为接口
+ */
+public interface IEatAnimalAction {
+
+    void eat();
+}
---
+++ design-pattern/src/main/java/com/glw/design/principle/interfaceisolation/ISwimAnimalAction.java
@@ -0,0 +1,12 @@
+package com.glw.design.principle.interfaceisolation;
+
+/**
+ * @author : glw
+ * @date : 2019/9/16
+ * @time : 10:01
+ * @Description : 接口隔离原则：动物游泳行为接口
+ */
+public interface ISwimAnimalAction {
+
+    void swim();
+}
