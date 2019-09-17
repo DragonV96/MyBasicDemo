75953eef5e63b3249bb94b5cf8c445deeca88458
glw <guolongwei@comtom.cn>
2019-09-17 17:44:23 +0800

    【添加】创建型——简单工厂实例

---
+++ design-pattern/src/main/java/com/glw/design/pattern/creational/simplefactory/PythonVideo.java
@@ -0,0 +1,14 @@
+package com.glw.design.pattern.creational.simplefactory;
+
+/**
+ * @author : glw
+ * @date : 2019/9/17
+ * @time : 16:56
+ * @Description : 简单工厂模式（非23种GOF）——创建型：python视频类
+ */
+public class PythonVideo extends Video{
+    @Override
+    public void produce() {
+        System.out.println("录制Python课程视频。");
+    }
+}
---
+++ design-pattern/src/main/java/com/glw/design/pattern/creational/simplefactory/VideoFactory.java
@@ -0,0 +1,38 @@
+package com.glw.design.pattern.creational.simplefactory;
+
+/**
+ * @author : glw
+ * @date : 2019/9/17
+ * @time : 17:00
+ * @Description : 简单工厂模式（非23种GOF）——创建型：视频工厂类
+ */
+public class VideoFactory {
+
+    /**
+     * 通过反射获取对象
+     * @param clazz
+     * @return
+     */
+    public Video getVideo(Class clazz){
+        Video video = null;
+        try {
+            video = (Video) Class.forName(clazz.getName()).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return video;
+    }
+
+    public Video getVideo(String type){
+        if ("java".equalsIgnoreCase(type)) {
+            return new JavaVideo();
+        } else if ("python".equalsIgnoreCase(type)) {
+            return new PythonVideo();
+        }
+        return null;
+    }
+}
